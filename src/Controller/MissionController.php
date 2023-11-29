0aaa879b208faabfec162cf7941bc92da37da2e6
Yamnyr <quentinwaroquet@gmail.comm>
2023-11-29 16:45:41 +0100

    header + droit admin

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/mission')]
+#[Route('/mission/mission')]
 class MissionController extends AbstractController
 {
     #[Route('/', name: 'app_mission_index', methods: ['GET'])]
